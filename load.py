2e870aac885c8b2237c636f88775f49a7297c082
Faiz Rahmadani <fzrahmadan@gmail.com>
2024-06-03 00:33:22 +0700

    adding tflite to the model.ipynb

---
+++ load.py
@@ -0,0 +1,44 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+import cv2
+import easygui
+
+
+# Load the TFLite model
+interpreter = tf.lite.Interpreter(model_path='02062024-234854.tflite')
+interpreter.allocate_tensors()
+
+# Get input and output tensors
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+
+input_path = easygui.fileopenbox(title='Select Image File')
+# img = image.load_img(test_image_path)
+img = cv2.imread(str(input_path))
+img = cv2.resize(img, (200, 200))
+
+
+# Convert the image to a numpy array
+X = tf.keras.preprocessing.image.img_to_array(img)
+X = np.expand_dims(X, axis=0)
+
+
+# Prepare the input tensor
+interpreter.set_tensor(input_details[0]['index'], X)
+
+# Invoke the interpreter
+interpreter.invoke()
+
+# Get the output
+val = interpreter.get_tensor(output_details[0]['index'])
+
+
+# print(val)
+# Interpret the output
+if val[0][0] >= 0.5:
+    print(f'unripe {val[0][0]}')
+else:
+    print(f'ripe {val[0][0]}')
